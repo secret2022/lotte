38f416a16f47bf1ce0855b96a8241c22c7ac941e
secret2022 <72841418+secret2022@users.noreply.github.com>
2021-04-02 10:41:22 +0900

    10:41 행바꿈

--- Day0329/src/net/hb/day29/Test.java
+++ Day0329/src/net/hb/day29/Test.java
@@ -1,15 +1,15 @@
 package net.hb.day29;
 
+
+
 public class Test {
 	
 	public static void main(String[] args) {
-		for(int i=1;i<10;i++) {
+		for(int i=2;i<10;i++) {
+				System.out.println();
 			for(int j=1;j<10;j++) {
-				System.out.println(i+"*"+j +"="+ i*j);
+				System.out.print(i+"*"+j +"="+ i*j+" ");
 			}//end
 		}//end
-		
 	}//end
-	
-
 }//END
